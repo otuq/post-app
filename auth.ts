8f075950e403052442da58ea838ced11915f1012
otuq <houtansannukicoladesuka@gmai.com>
2026-06-10 15:15:14 +0900

    remove email auth provider

--- auth.ts
+++ auth.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import GitHub from "next-auth/providers/github";
-import Nodemailer from "next-auth/providers/nodemailer";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
@@ -9,15 +8,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(prisma),
     providers: [
         Google({
-            //googleでもgituhubでもログインできるが、安全面上は外すのが無難らしい
             allowDangerousEmailAccountLinking: true,
         }),
         GitHub({
             allowDangerousEmailAccountLinking: true,
-        }),
-        Nodemailer({
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM,
         }),
     ],
     session: {
