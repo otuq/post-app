baf43284644d07e3999c539f729ea8bce092c5c4
otuq <houtansannukicoladesuka@gmai.com>
2026-06-13 14:55:27 +0900

    fixed sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@
 import type { MetadataRoute } from "next"
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = "https://あなたのvercel-url.vercel.app"
+    const baseUrl = "https://https://post-app-ruby.vercel.app/-url.vercel.app"
 
     return [
         {
